df1f63b934ed42e38d930a16b00e74b2034ba58a
FullCycleRoid <andrewthechf@gmail.com>
2019-12-27 15:46:49 +0300

    new models

--- tasker/main/models.py
+++ tasker/main/models.py
@@ -1,31 +1,30 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
 
-"User section"
-class User():
-    """New user model"""
-    pass
+
+# USER SECTION
+
+# class User():
+#     """New user model"""
+#     pass
 
 class UserProfile(models.Model):
     """User additional information"""
-    pass
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_image = models.ImageField(upload_to='user/profile_image', default='img/default_image.jpg')
+    name = models.CharField(max_length=50)
 
 
+# TASKBOARD SECTION
 
-"TASKBOARD SECTION"
-state = {
-    'x':'red',
-    'done':'green',
-    'in_progress': 'purple'
-}
 
-class TaskBoard(models.Model):
-    """Main task board info"""
-    name = models.CharField(max_length=40)
-    # user = models.ForeignKey()
+class Goal(models.Model):
+    """User goals and preferred accomplishments"""
+    title = models.CharField(max_length=50)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     description = models.TextField(max_length=2000)
-    condition = models.CharField(max_length=1, choices=state, default=state['x'])
-    created_at = models.DateTimeField()
+    created_at = models.DateTimeField(editable=False)
     last_modified = models.DateTimeField()
 
     def save(self, *args, **kwargs):
@@ -36,13 +35,47 @@ class TaskBoard(models.Model):
         return super().save(*args, **kwargs)
 
 
-"""Rating system section"""
+state = {
+    'x': 'red',
+    'done': 'green',
+    'in_progress': 'purple'
+}
+
+
+class DailyTask(models.Model):
+    """Daily task"""
+    goals = models.ForeignKey('Goal', on_delete=models.PROTECT)
+    condition = models.CharField(max_length=1, choices=state, default=state['x'])
+
+    def save(self, *args, **kwargs):
+        """On save, update timestamps"""
+        if not self.pk:
+            self.created_at = timezone.now()
+        self.last_modified = timezone.now()
+        return super().save(*args, **kwargs)
+
+
+class Note(models.Model):
+    """Useful redactor for notes"""
+    name = models.CharField(max_length=20)
+    body = models.TextField()
+
+
+# RATING SYSTEM SECTION
 class Rating(models.Model):
     """Implementation of rating system model"""
-    pass
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    month_achievement = models.CharField(max_length=50)
 
 
-"""Schedule section"""
+# COMMENTS SECTION
+class Comment(models.Model):
+    text = models.CharField(max_length=500)
+    taskboard = models.ForeignKey('TaskBoard', on_delete=models.CASCADE)
+
+
+# SCHEDULE SECTION
 class Schedule():
     """"""
     pass
