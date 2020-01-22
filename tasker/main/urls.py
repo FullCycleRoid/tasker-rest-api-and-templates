d995cb9d1c212137d6ea0d3c9ad6794a242699d2
Kirill <andrewthechf@gmail.com>
2020-01-23 02:20:16 +0300

    Models update commit

--- tasker/main/urls.py
+++ tasker/main/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
+from django.urls import path
 from main.views import main_board, TaskDetail, RegView
 
 app_name = 'main'
 
 urlpatterns = [
     url('^detail/(?P<pk>\d+)/$', TaskDetail.as_view(), name='detail'),
-    url('^registration/$', RegView.as_view(), name='invited_user_registration'),
+    url('registration/$', RegView.as_view(), name='invited_user_registration'),
     url('', main_board, name='main_board'),
 ]
 
